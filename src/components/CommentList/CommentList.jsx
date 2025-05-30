6acee4db57cf0e038bb7401d5686fd5a42316308
Maryna <doctor.who2016.dw@gmail.com>
2025-05-30 18:04:48 +0300

    react statis list of posts

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,9 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    {comments.map(comment => (
+      <CommentInfo key={comments.id} comment={comment} />
+    ))}
+  </div>
+);
